d49dcb30c32247533a4cfac07afcd3a3bf9c5597
Abdulrahman-2005 <bodyazmy.new.2005@gmail.com>
2021-11-04 01:20:29 +0200

    fixed a ton of shit, added meta tags: and more

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ let nationalAnthemEl = document.getElementById("anthem");
 let audioStatus = document.getElementById("status");
 let title = document.getElementById("title");
 let settings = document.getElementById("settings-menu");
-
 let mapPath = "assets/maps/world.jpg";
 let flagpath = "assets/flags/world.svg";
 let anthem;
@@ -58,101 +57,52 @@ function append() {
 append();
 
 function check() {
-	if (countries[countriesSelect.value]["capital"] === undefined) {
-		countries[countriesSelect.value]["capital"] = "لا توجد معلومات";
+	selection = countries[countriesSelect.value];
+	if (selection["capital"] === undefined) {
+		selection["capital"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["area"] === undefined) {
-		countries[countriesSelect.value]["area"] = "لا توجد معلومات";
+	if (selection["area"] === undefined) {
+		selection["area"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["population"] === undefined) {
-		countries[countriesSelect.value]["population"] = "لا توجد معلومات";
+	if (selection["population"] === undefined) {
+		selection["population"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["language"] === undefined) {
-		countries[countriesSelect.value]["language"] = "لا توجد معلومات";
+	if (selection["language"] === undefined) {
+		selection["language"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["relegion"] === undefined) {
-		countries[countriesSelect.value]["relegion"] = "لا توجد معلومات";
+	if (selection["relegion"] === undefined) {
+		selection["relegion"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["currency"] === undefined) {
-		countries[countriesSelect.value]["currency"] = "لا توجد معلومات";
+	if (selection["currency"] === undefined) {
+		selection["currency"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["capital"] === "") {
-		countries[countriesSelect.value]["capital"] = "لا توجد معلومات";
+	if (selection["capital"] === "") {
+		selection["capital"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["importantPort"] === "") {
-		countries[countriesSelect.value]["importantPort"] = "لا توجد معلومات";
+	if (selection["importantPort"] === "") {
+		selection["importantPort"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["population"] === "") {
-		countries[countriesSelect.value]["population"] = "لا توجد معلومات";
+	if (selection["population"] === "") {
+		selection["population"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["language"] === "") {
-		countries[countriesSelect.value]["language"] = "لا توجد معلومات";
+	if (selection["language"] === "") {
+		selection["language"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["relegion"] === "") {
-		countries[countriesSelect.value]["relegion"] = "لا توجد معلومات";
+	if (selection["relegion"] === "") {
+		selection["relegion"] = "لا توجد معلومات";
 	}
-	if (countries[countriesSelect.value]["currency"] === "") {
-		countries[countriesSelect.value]["currency"] = "لا توجد معلومات";
+	if (selection["currency"] === "") {
+		selection["currency"] = "لا توجد معلومات";
 	}
 }
 
 function run() {
 	countriesSelect.innerHTML = '<option value="choose">اختر دولة</option>';
 
-	if (continentsSelect.value === "africa") {
-		for (i in continents["africa"]) {
-			countriesSelect.add(
-				new Option(
-					continents["africa"][i]["arname"],
-					continents["africa"][i]["ename"]
-				)
-			);
-		}
-	} else if (continentsSelect.value === "asia") {
-		for (i in continents["asia"]) {
-			countriesSelect.add(
-				new Option(
-					continents["asia"][i]["arname"],
-					continents["asia"][i]["ename"]
-				)
-			);
-		}
-	} else if (continentsSelect.value === "europe") {
-		for (i in continents["europe"]) {
-			countriesSelect.add(
-				new Option(
-					continents["europe"][i]["arname"],
-					continents["europe"][i]["ename"]
-				)
-			);
-		}
-	} else if (continentsSelect.value === "northAmirca") {
-		for (i in continents["northAmirca"]) {
-			countriesSelect.add(
-				new Option(
-					continents["northAmirca"][i]["arname"],
-					continents["northAmirca"][i]["ename"]
-				)
-			);
-		}
-	} else if (continentsSelect.value === "southAmirca") {
-		for (i in continents["southAmirca"]) {
-			countriesSelect.add(
-				new Option(
-					continents["southAmirca"][i]["arname"],
-					continents["southAmirca"][i]["ename"]
-				)
-			);
-		}
-	} else if (continentsSelect.value === "oceania") {
-		for (i in continents["oceania"]) {
-			countriesSelect.add(
-				new Option(
-					continents["oceania"][i]["arname"],
-					continents["oceania"][i]["ename"]
-				)
-			);
-		}
+	con = continentsSelect.value;
+	for (i in continents[con]) {
+		newOption = `<option value="${continents[con][i]["ename"]}">${continents[con][i]["arname"]}</option>`;
+		countriesSelect.innerHTML += newOption;
 	}
 }
 
@@ -186,7 +136,6 @@ function set() {
 		`${assets}maps/${countriesSelect.value}.jpg` ||
 		`${assets}maps/${countriesSelect.value}.jpg`;
 	flagPath = `${assets}flags/${countriesSelect.value}.svg`;
-
 	flag.src = flagPath;
 	map.src = mapPath;
 
@@ -220,26 +169,30 @@ function set() {
 	} - الموسوعة الجغرافية`;
 }
 
-let x = 0;
-function runPause() {
-	if (x === 0) {
-		playAnthem();
-		audioStatus.innerText = "playing";
-		x = 1;
-	} else {
-		pauseAnthem();
-		audioStatus.innerText = "paused";
-		x = 0;
-	}
-}
 
 function playAnthem() {
+	audioStatus.textContent = "playing";
 	anthem.play();
 }
 
 function pauseAnthem() {
+	audioStatus.textContent = "paused";
 	anthem.pause();
 }
+
+let x = 0;
+function runPause() {
+	if (x === 0) {
+		playAnthem();
+
+		x = 1;
+	} else {
+		pauseAnthem();
+		x = 0;
+	}
+}
+
+flag.addEventListener("click", runPause)
 
 map.onclick = function resize() {
 	if (y === 1) {
